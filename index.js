5b331b33ca80405a016c3ca46d972bac48758de3
ajit kerle <ajitkerle70@gmail.com>
2022-12-30 17:22:35 +0530

    l

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use("/",router)
 
 app.use(express.static(path.join(__dirname,"./client/build")))
 app.get("*",(req,res)=>{
-   res.sendFile(path.join(__dirname,"./client/build/index.html"))
+   res.sendFile(path.join(__dirname,"./client/build/index.html"),(err)=>{
+    res.status(500).send(err)
+   })
 })
 
 
